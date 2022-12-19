360bc6ce6e6fb681527957275a141adffbd4fe1d
Jurriaanmk <90131419+Jurriaanmk@users.noreply.github.com>
2022-12-19 15:27:56 +0100

    .

--- vision_3/Week 5/Opgaves_Week_5.py
+++ vision_3/Week 5/Opgaves_Week_5.py
@@ -1,24 +1,66 @@
-#%% Opgave 2 bepaal afgeleide 1 afbeelding op basis van de Disney.jpg
+#%% Opgave 1 
 
 import numpy as np
 import cv2 
-
-Afbeelding_Disney = cv2.imread('Disney.jpg', 0)
-Afbeelding_Disney = cv2.cvtColor(Afbeelding_Disney, cv2.COLOR_BGR2GRAY)
-Afbeelding_Disney_Mediaan = cv2.medianBlur(Afbeelding_Disney,0.5)
+from scipy import signal
+import matplotlib.pyplot as plt
 
 
-rows, colloms = Afbeelding_Disney_Mediaan[:2]
-Afbeelding_Disney_Diff_x = numpy.diff(rows)
-Afbeelding_Disney_Diff_y = numpy.diff(colloms)
-Afbeelding_Disney_Diff = Afbeelding_Disney_Diff_y/Afbeelding_Disney_Diff_x
+Afbeelding_Disney = cv2.imread('Disney.jpg')
+gray = cv2.cvtColor(Afbeelding_Disney, cv2.COLOR_BGR2GRAY)
 
-#%%
+kernel_x = np.array([[-1,0,1],
+                     [-2,0,2],
+                     [-1,0,1]])
 
-for i in range(rows):
-    for j in range(colloms):
-        
-        
+derivative = signal.convolve2d(gray, kernel_x, boundary='symm', mode='same')
+
+gradient = np.sqrt(derivative**2)
+
+gradient = gradient / np.max(gradient)
+threshold = 0.5
+edge_map = (gradient > threshold).astype(np.uint8) * 255
+
+low_threshold = 50
+high_threshold = 150
+edge_map = cv2.Canny(gray, low_threshold, high_threshold)
+
+kernel = np.array([[0, 1, 0],
+                   [1, -4, 1],
+                   [0, 1, 0]])
+
+
+laplacian = signal.convolve2d(gray, kernel, boundary='symm', mode='same')
+
+threshold = 50
+edge_map = (laplacian > threshold).astype(np.uint8) * 255
+
+
+fig, axs = plt.subplots(2, 2)
+
+# Display the original image
+axs[0, 0].imshow(Afbeelding_Disney)
+axs[0, 0].set_title('Original Image')
+
+# Display the derivative
+axs[0, 1].imshow(derivative, cmap='gray')
+axs[0, 1].set_title('Derivative')
+
+# Display the gradient magnitude
+axs[1, 0].imshow(gradient, cmap='gray')
+axs[1, 0].set_title('Gradient Magnitude')
+
+# Display the edge map
+axs[1, 1].imshow(edge_map, cmap='gray')
+axs[1, 1].set_title('Edge Map')
+
+# Show the figure
+plt.show()
 
 
 
+
+
+
+
+# %%
