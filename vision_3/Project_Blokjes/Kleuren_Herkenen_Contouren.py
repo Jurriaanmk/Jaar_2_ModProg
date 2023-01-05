bc8075c88a4db5f2c0e3ba20e577894252cf6e02
Jurriaanmk <90131419+Jurriaanmk@users.noreply.github.com>
2023-01-05 17:16:57 +0100

    .

--- vision_3/Project_Blokjes/Kleuren_Herkenen_Contouren.py
+++ vision_3/Project_Blokjes/Kleuren_Herkenen_Contouren.py
@@ -1,33 +1,70 @@
-#%%
+#%% Detectie blauw
 import cv2
 import numpy as np
 
 # Load the image
 img = cv2.imread('gekleurde_blokjes.jpg')
-
 # Convert the image to the HSV color space
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
+lower_color_Wit = np.array([101, 56, 250])
+upper_color_Wit = np.array([0, 0, 255])
+
+
 # Define the range of colors to detect
-lower_color = np.array([100, 50, 50])
-upper_color = np.array([140, 255, 255])
+lower_color_blue = np.array([100, 0, 0])
+upper_color_blue = np.array([109, 217, 255])
 
 # Create a mask that only includes pixels within the defined color range
-mask = cv2.inRange(hsv, lower_color, upper_color)
+mask_blauw = cv2.inRange(hsv, lower_color_blue, upper_color_blue)
+mask_Wit = cv2.inRange(hsv,lower_color_Wit,upper_color_Wit)
 
 # Apply the mask to the original image
-res = cv2.bitwise_and(img, img, mask=mask)
+res_blauw = cv2.bitwise_and(img, img, mask=mask_blauw)
+res_Wit = cv2.bitwise_and(img, img, mask=mask_Wit)
 
 # Find the contours of the colored shapes in the image
-contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+contours_blauw, _ = cv2.findContours(mask_blauw, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+contours_Wit, _ = cv2.findContours(mask_Wit, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
 # Draw the contours onto the original image
-cv2.drawContours(img, contours, -1, (0, 255, 0), 2)
+cv2.drawContours(img, contours_blauw, -1, (0, 255, 0), 2)
+cv2.drawContours(img, contours_Wit, -1, (0, 255, 0), 2)
+
 
 # Show the image
+#cv2.imshow('mask_blauw', mask_blauw)
+cv2.imshow('mask_wit', mask_Wit)
 cv2.imshow('Image', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+#%%
+
+import cv2
+import numpy as np
+
+
+rgb_blauw_laag = np.uint8([[[190,101,28 ]]])
+rgb_blauw_Hoog = np.uint8([[[255,115,38 ]]])
+
+rgb_Wit_laag = np.uint8([[[250,230,195 ]]])
+rgb_Wit_Hoog = np.uint8([[[255,255,255 ]]])
+
+hsv_blauw_laag = cv2.cvtColor(rgb_blauw_laag,cv2.COLOR_BGR2HSV)
+hsv_blauw_Hoog = cv2.cvtColor(rgb_blauw_Hoog,cv2.COLOR_BGR2HSV)
+
+hsv_Wit_laag = cv2.cvtColor(rgb_Wit_laag,cv2.COLOR_BGR2HSV)
+hsv_Wit_Hoog = cv2.cvtColor(rgb_Wit_Hoog,cv2.COLOR_BGR2HSV)
+
+print(hsv_blauw_laag)
+print(hsv_blauw_Hoog)
+
+print(hsv_Wit_laag)
+print(hsv_Wit_Hoog)
+
+
+
 
 #%%
 import cv2
@@ -176,5 +213,273 @@ for contour in contours:
 cv2.imshow('Image', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+# %%
+import cv2
+import numpy as np
+
+# Load the image
+image = cv2.imread('gekleurde_blokjes.jpg')
+
+# Convert the image to grayscale
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Blur the image to reduce high frequency noise
+blurred_image = cv2.GaussianBlur(gray_image, (5, 5), 0)
+
+# Perform Canny edge detection
+edges = cv2.Canny(blurred_image, 50, 150)
+
+# Find contours in the image
+contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+# Iterate through the contours and draw them on the original image
+for contour in contours:
+    cv2.drawContours(image, [contour], -1, (0, 255, 0), 2)
+
+# Show the image
+cv2.imshow('Shapes', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# %%
+import cv2
+import numpy as np
+
+# Load the image
+image = cv2.imread('gekleurde_blokjes.jpg')
+
+# Convert the image to grayscale
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Blur the image to reduce high frequency noise
+blurred_image = cv2.GaussianBlur(gray_image, (5, 5), 0)
+
+cv2.imshow('Non-blurred_image ', blurred_image)
+
+
+# Perform Canny edge detection
+edges = cv2.Canny(blurred_image, 50, 150)
+
+# Find contours in the image
+contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_TC89_KCOS)
+
+# Iterate through the contours and draw them on the original image
+for contour in contours:
+    # Check if the contour is a square
+    if not cv2.isContourConvex(contour):
+        # Draw the contour on the image
+        cv2.drawContours(image, [contour], -1, (0, 255, 0), 2)
+
+# Show the image
+cv2.imshow('Non-Perfect Squares', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# %%
+import cv2
+
+img = cv2.imread("gekleurde_blokjes.jpg") # Read image
+
+# Setting parameter values
+t_lower = 50 # Lower Threshold
+t_upper = 150 # Upper threshold
+
+# Applying the Canny Edge filter
+edge = cv2.Canny(img, t_lower, t_upper)
+
+cv2.imshow('original', img)
+cv2.imshow('edge', edge)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+#%%
+import cv2
+import numpy as np
+
+# Load the image and convert it to grayscale
+image = cv2.imread('gekleurde_blokjes.jpg')
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Use the Canny function to detect edges in the image
+edges = cv2.Canny(gray, 10, 150)
+
+# Find the contours in the image
+contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+# Iterate through the contours and draw them on the image
+for contour in contours:
+    if len(contour) == 4:
+        # This is a square, so draw it in yellow
+        cv2.drawContours(image, [contour], 0, (0, 255, 255), 2)
+    elif len(contour) == 3:
+        # This is a triangle, so draw it in red
+        cv2.drawContours(image, [contour], 0, (0, 0, 255), 2)
+    elif len(contour) > 10:
+        # This is some other shape, so draw it in green
+        cv2.drawContours(image, [contour], 0, (0, 255, 0), 2)
+
+# Show the image
+cv2.imshow('Image', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# %%
+import cv2
+import numpy as np
+
+# Load the image and convert it to grayscale
+image = cv2.imread('gekleurde_blokjes.jpg')
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Use thresholding to create a binary image
+threshold, binary_image = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY)
+
+# Find the contours in the image
+contours, _ = cv2.findContours(binary_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+# Iterate through the contours and approximate them with polygons
+for contour in contours:
+    epsilon = 0.01 * cv2.arcLength(contour, True)
+    approx = cv2.approxPolyDP(contour, epsilon, True)
+    
+    # Check the number of points in the approximated polygon
+    if len(approx) == 4:
+        # This is a square, so draw it in yellow
+        cv2.drawContours(image, [approx], 0, (0, 255, 255), 2)
+    elif len(approx) == 3:
+        # This is a triangle, so draw it in red
+        cv2.drawContours(image, [approx], 0, (0, 0, 255), 2)
+    elif len(approx) > 10:
+        # This is a larger shape, so draw it in green
+        cv2.drawContours(image, [approx], 0, (0, 255, 0), 2)
+
+# Show the image
+cv2.imshow('Image', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# %%
+import cv2
+import numpy as np
+image_obj = cv2.imread('gekleurde_blokjes.jpg')
+
+gray = cv2.cvtColor(image_obj, cv2.COLOR_BGR2GRAY)
+
+kernel = np.ones((4, 4), np.uint8)
+dilation = cv2.dilate(gray, kernel, iterations=1)
+
+blur = cv2.GaussianBlur(dilation, (5, 5), 0)
+
+
+thresh = cv2.adaptiveThreshold(blur, 255, 1, 1, 11, 2)
+
+# Now finding Contours         ###################
+contours, _ = cv2.findContours(
+    thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+coordinates = []
+for cnt in contours:
+        # [point_x, point_y, width, height] = cv2.boundingRect(cnt)
+    approx = cv2.approxPolyDP(
+        cnt, 0.07 * cv2.arcLength(cnt, True), True)
+    if len(approx) == 3:
+        coordinates.append([cnt])
+        cv2.drawContours(image_obj, [cnt], 0, (0, 0, 255), 3)
+
+cv2.imwrite("result.png", image_obj)
+cv2.imshow('Image', image_obj)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+# %%
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+# reading image
+img = cv2.imread('gekleurde_blokjes.jpg')
+
+# converting image into grayscale image
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# setting threshold of gray image
+_, threshold = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+
+# using a findContours() function
+contours, _ = cv2.findContours(
+	threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+i = 0
+
+# list for storing names of shapes
+for contour in contours:
+
+	# here we are ignoring first counter because
+	# findcontour function detects whole image as shape
+	if i == 0:
+		i = 1
+		continue
+
+	# cv2.approxPloyDP() function to approximate the shape
+	approx = cv2.approxPolyDP(
+		contour, 0.01 * cv2.arcLength(contour, True), True)
+	
+	# using drawContours() function
+	cv2.drawContours(img, [contour], 0, (0, 0, 255), 5)
+
+	# finding center point of shape
+	M = cv2.moments(contour)
+	if M['m00'] != 0.0:
+		x = int(M['m10']/M['m00'])
+		y = int(M['m01']/M['m00'])
+
+	# putting shape name at center of each shape
+	if len(approx) == 3:
+		cv2.putText(img, 'Triangle', (x, y),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+
+	elif len(approx) == 4:
+		cv2.putText(img, 'Quadrilateral', (x, y),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+
+	elif len(approx) == 5:
+		cv2.putText(img, 'Pentagon', (x, y),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+
+	elif len(approx) == 6:
+		cv2.putText(img, 'Hexagon', (x, y),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+
+	else:
+		cv2.putText(img, 'circle', (x, y),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+
+# displaying the image after drawing contours
+cv2.imshow('shapes', img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# %%
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+# reading image
+img = cv2.imread('gekleurde_blokjes.jpg')
+
+# converting image into grayscale image
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# setting threshold of gray image
+_, threshold = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+
+# using a findContours() function
+contours, _ = cv2.findContours(
+	threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+cv2.drawContours(img, contours, -1, (0, 255, 0), 2)
+
+cv2.imshow('shapes', img)
 
 # %%
