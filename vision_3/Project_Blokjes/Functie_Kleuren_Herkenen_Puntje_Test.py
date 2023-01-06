62b0e14e659e446792a9e1acd4819bea72de9e80
Jurriaanmk <90131419+Jurriaanmk@users.noreply.github.com>
2023-01-06 12:11:49 +0100

    Final version

--- vision_3/Project_Blokjes/Functie_Kleuren_Herkenen_Puntje_Test.py
+++ vision_3/Project_Blokjes/Functie_Kleuren_Herkenen_Puntje_Test.py
@@ -1,10 +1,10 @@
 #%%
 #Code om de contouren te herkenen op basis van kleur. Geen vorm herkening.
+#Code moet nog aangepast worden, op moment bij enkele kleuren een error dat er gedeeld wordt door 0, komt waarschijnlijk omdat er niet een perfect middenpunt is
 import cv2
 import numpy as np
-from typing import Tuple
 
-Middenpunt_Print = 1 #1 = aan, 0 = uit. Print in de console de middenpunt waarde, kan gebruikt worden voor het exporteren van de middenpunten van de blokjes
+Middenpunt_Weergeven = 1
 
 Afbeelding = cv2.imread('gekleurde_blokjes.jpg')
 Afbeelding_HSV = cv2.cvtColor(Afbeelding, cv2.COLOR_BGR2HSV)
@@ -12,47 +12,26 @@ Afbeelding2 = Afbeelding.copy()
 Afbeelding2_HSV = cv2.cvtColor(Afbeelding2, cv2.COLOR_BGR2HSV)
 
 Kleuren_Bieb = {    #Bieb voor alle kleuren
-        "Rood": {
-            "Waardes":(np.array([163, 155, 181]), np.array([179, 213, 223])),
-            "Voorgekomen": 0
-        },
-        "Wit": {
-            "Waardes": (np.array([85, 9, 223]), np.array([121, 62, 255])),
-            "Voorgekomen": 0
-        },
-        "Blauw":{
-            "Waardes":(np.array([90, 111, 196]), np.array([113, 233, 232])),
-            "Voorgekomen": 0
-        },
-        "Paars":{
-            "Waardes":(np.array([122, 121, 169]), np.array([130, 173, 204])),
-            "Voorgekomen": 0
-        },
-        "Oranje":{
-            "Waardes":(np.array([0, 121, 157]), np.array([7, 159, 231])),
-            "Voorgekomen": 0
-        },
-        "Groen":{
-            "Waardes":(np.array([70, 190, 84]),np.array([97, 255, 255])),
-            "Voorgekomen": 0
-        },
-        "Geel":{
-            "Waardes":(np.array([22, 241, 201]), np.array([30, 255, 225])),
-            "Voorgekomen": 0
-        },
+        "Rood": (np.array([163, 155, 181]), np.array([179, 213, 223])),
+        "Wit": (np.array([85, 9, 223]), np.array([121, 62, 255])),
+        "Blauw":(np.array([90, 111, 196]), np.array([113, 233, 232])),
+        "Paars":(np.array([122, 121, 169]), np.array([130, 173, 204])),
+        "Oranje":(np.array([0, 121, 157]), np.array([7, 159, 231])),
+        "Groen": (np.array([70, 190, 84]),np.array([97, 255, 255])),
+        "Geel": (np.array([22, 241, 201]), np.array([30, 255, 225])),
     }
 
-def Verkrijg_kleur_randwaardes(kleur: str) -> Tuple[np.ndarray, np.ndarray]: #functie om alle kleuren optehalen
-    return Kleuren_Bieb[kleur]["Waardes"]
+def Verkrijg_kleur_randwaardes(kleur): #functie om alle kleuren optehalen
+    return Kleuren_Bieb[kleur]
 
-def Middenpunt_Bepalen(Contour,index: int) -> Tuple[int,int]: # Functie om te bepalen wat de middenpunten van alle objecten zijn en hier een stipje te zetten en de coordinaten te exporteren
+def Middenpunt_Bepalen(Contour,index): # Functie om te bepalen wat de middenpunten van alle objecten zijn en hier een stipje te zetten en de coordinaten te exporteren
     moments = cv2.moments(Contour[index])
     Middenpunt_x = int(moments['m10'] / moments['m00'])
     Middenpunt_y = int(moments['m01'] / moments['m00'])
-    cv2.circle(Afbeelding, (Middenpunt_x, Middenpunt_y), 5, (0, 0, 0), -1)
+    print("Middenpunt: (", Middenpunt_x, ", ", Middenpunt_y, ")")
 
-    if Middenpunt_Print:
-        print("Middenpunt: (", Middenpunt_x, ", ", Middenpunt_y, ")")
+    if Middenpunt_Weergeven:
+          cv2.circle(Afbeelding, (Middenpunt_x, Middenpunt_y), 5, (0, 0, 0), -1)
 
     return Middenpunt_x, Middenpunt_y
 
@@ -76,10 +55,6 @@ def Kleuren_Herken(Kleur): #Hoofdfunctie om de kleuren te zoeken en de contours 
 
         areas = [cv2.contourArea(c) for c in Basis_contours] #Zoek de contouren in alle contouren
         max_index = np.argmax(areas) #Selecteerd de grootste contour uit alle contouren
-        if max_index == 0:
-            Kleuren_Bieb[Kleur]["Voorgekomen"] += 1
-            print("Aantal keer voorgekomen: ",Kleuren_Bieb[Kleur]["Voorgekomen"])
-
 
         cv2.drawContours(Afbeelding, Basis_contours,max_index, (0, 0, 0), 2) #Teken een contour in Zwart rond de grootste contour
 
